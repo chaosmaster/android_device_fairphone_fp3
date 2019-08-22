erfanoabdi/android_device_motorola_river_prebuiltvendor

river: add TWRP installer

BoardConfig.mk
@@ -82,3 +82,7 @@ TW_H_OFFSET := -110
 # Debug flags
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
+
+# Installer
+USE_RECOVERY_INSTALLER := true
+RECOVERY_INSTALLER_PATH := device/motorola/evert/installer

chaosmaster/android_device_fairphone_fp3

BoardConfig.mk
@@ -79,3 +79,7 @@ TW_H_OFFSET := -110
 # Debug flags
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
+
+# Installer
+USE_RECOVERY_INSTALLER := true
+RECOVERY_INSTALLER_PATH := device/motorola/river/installer